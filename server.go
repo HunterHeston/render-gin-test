d99a2c10fff1c0ec1703d560733f02e1cc2d6b46
Hunter Heston <huntersamuelheston@gmail.com>
2021-11-26 20:18:03 -0800

    init

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hunterheston/gin-server/routes"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/home", routes.Home)
+	r.NoRoute(routes.Redirect)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
