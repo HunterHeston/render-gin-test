4959338e6cefd20236d20ef69b7cf46e10253bce
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-01 19:20:06 -0800

    removed initial implementation of createurl added comments about initalization of datastores

--- routes/createurl.go
+++ routes/createurl.go
@@ -1,61 +1,19 @@
 package routes
 
 import (
-	"fmt"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-var database = make(map[string]string)
-
 // main route handler
 func CreateURL(c *gin.Context) {
-	ds := dataStore{
-		store: database,
-	}
-
-	fmt.Printf("Data store: %v\n", ds)
-
-	url := c.Param("url")
-	shortURL, err := ds.createurl(url)
-	if err != nil {
-		fmt.Println("Error getting short url: ", err)
-	}
-
-	fmt.Printf("Orignial url: %q short url: %q\n", url, shortURL)
 
 	c.JSON(http.StatusOK, gin.H{
-		"shortURL": shortURL,
+		"shortURL": "",
 	})
 }
 
 type dataStore struct {
 	store map[string]string
-}
-
-func (ds dataStore) createurl(url string) (string, error) {
-	if ds.store == nil {
-		return "", fmt.Errorf("data store is nil")
-	}
-
-	if !validateURL(url) {
-		return "", fmt.Errorf("url %q is invalid", url)
-	}
-
-	hash := rand.Intn(100)
-	ds.store[strconv.FormatInt(int64(hash), 10)] = url
-
-	return fmt.Sprintf("localhost:8080/%d", hash), nil
-}
-
-func (ds dataStore) lookupURL(hash string) (string, error) {
-	return ds.store[hash], nil
-}
-
-func validateURL(url string) bool {
-
-	return true
 }
--- server.go
+++ server.go
@@ -5,6 +5,14 @@ import (
 	"github.com/hunterheston/gin-server/routes"
 )
 
+func init() {
+	// initialize an in memory data store.
+	// make routes initializable.
+	// allow passing of value into a route structure.
+	// need to pass the data store to a route.
+	// So that both CreateURL and Redirect URL can access the same in memory data store.
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/create-url/:url", routes.CreateURL)
