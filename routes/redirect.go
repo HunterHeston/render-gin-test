4ac8bbec6aa4f3611297d822f59884577ea2bde0
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-03 20:00:52 -0800

    removed garbage from redirect. Firebase implementation to follow.

--- routes/redirect.go
+++ routes/redirect.go
@@ -1,26 +1,11 @@
 package routes
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Redirect(c *gin.Context) {
-
-	ds := dataStore{
-		database,
-	}
-
-	hash := c.Param("hash")
-	url, err := ds.lookupURL(hash)
-	if err != nil {
-		fmt.Printf("could not find url for hash %q\n", hash)
-	}
-
-	fmt.Printf("hash is %q\n", hash)
-
 	c.JSON(200, gin.H{
-		"url": url,
+		"url": "url",
 	})
 }
