e3754baa473885f386fce8512b218db6522f934e
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-03 19:57:11 -0800

    Pulled a random string generation function from stack overflow. As is the law.

---
+++ helpers/string-generation/randeomstring.go
@@ -0,0 +1,14 @@
+package stringgeneration
+
+import "math/rand"
+
+// pulled from https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytesRmndr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
