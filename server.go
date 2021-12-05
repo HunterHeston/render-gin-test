733432e963bc4bb384d8fe29c4e12fe4bf60c566
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-05 15:48:16 -0800

    removed outdated comment

--- server.go
+++ server.go
@@ -8,14 +8,6 @@ import (
 	"github.com/hunterheston/gin-server/src/valuestore/inmemory"
 )
 
-func init() {
-	// initialize an in memory data store.
-	// make routes initializable.
-	// allow passing of value into a route structure.
-	// need to pass the data store to a route.
-	// So that both CreateURL and Redirect URL can access the same in memory data store.
-}
-
 var database valuestore.ValueStore
 
 func init() {
