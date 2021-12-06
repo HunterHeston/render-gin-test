73daa0dde2dd6625f782834289873706a9c58d9c
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-05 16:05:57 -0800

    logic for generating useful errors and validating urls before saving them.

--- src/routes/createurl/createurl.go
+++ src/routes/createurl/createurl.go
@@ -1,9 +1,12 @@
 package createurl
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hunterheston/gin-server/src/validation"
 	"github.com/hunterheston/gin-server/src/valuestore"
 )
 
@@ -16,27 +19,36 @@ func New(valueStore valuestore.ValueStore) func(c *gin.Context) {
 
 // main route handler
 func CreateURL(c *gin.Context) {
-	// route expects a single
+	// route expects a single param "url"
 	rawUrlInput := c.Query("url")
-	if !validUrl(rawUrlInput) {
-		// TODO: return an error to the client.
+
+	rawUrlInput = appendHTTP(rawUrlInput)
+
+	// must be a valid url.
+	if !validation.ValidateURL(rawUrlInput) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Input must be a valid url, %q is not valid.", rawUrlInput),
+		})
+		return
 	}
 
+	// Store the url and get it's id.
 	valueID, err := vs.Save([]byte(rawUrlInput))
 	if err != nil {
-		// TODO: return an error to the client.
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Error saving your input: %v", err),
+		})
 	}
 
+	// Send the ID of the stored value back to the client.
 	c.JSON(http.StatusOK, gin.H{
 		"id": valueID,
 	})
 }
 
-// check if string is a valid url.
-func validUrl(url string) bool {
-	return true
-}
-
-type dataStore struct {
-	store map[string]string
+func appendHTTP(url string) string {
+	if !strings.Contains(url, "http") {
+		return "http://" + url
+	}
+	return url
 }
