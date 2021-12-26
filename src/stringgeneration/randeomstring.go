f49f94e57cf8691a3444c6f68fe840603061df39
Hunter Heston <huntersamuelheston@gmail.com>
2021-12-26 10:56:40 -0800

    need to seed random number generators

--- src/stringgeneration/randeomstring.go
+++ src/stringgeneration/randeomstring.go
@@ -1,9 +1,16 @@
 package stringgeneration
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // pulled from https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func RandStringBytesRmndr(n int) string {
 	b := make([]byte, n)
